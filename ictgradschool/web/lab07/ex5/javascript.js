ece16e72e415bdfebdf412a3b5e19dbfa507f61b
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-12-02 09:45:41 +1300

    baubles falling on command.

--- ictgradschool/web/lab07/ex5/javascript.js
+++ ictgradschool/web/lab07/ex5/javascript.js
@@ -1,10 +1,34 @@
-let ready = $(document).ready(function () {
+$(document).ready(function () {
 
-    $('.bauble').on('hover', function () {
-        $('.bauble').css({
+    $('.bauble').on('click', function () {
+        $(this).addClass('baubleAni').css('animation-delay','10ms');
+        $(this).css('animation-fill-mode','forwards');
+    });
 
+    $('.bauble').hover(function () {
+        $(this).addClass('baubleAni').css('animation-delay','10ms');
+        $(this).css('animation-fill-mode','forwards');
+    }
+    ,function () {
+        window.setTimeout(retTheBall,3000);
         });
-    })
+
+    function retTheBall(){
+        $(this).css('animation-fill-mode','backwards');
+    }
 
 
 });
+
+
+
+
+
+$('.page').on('click',function () {
+    $(this).addClass('pageAnimation').css('animation-delay','10ms');
+    pageNo+=1;
+    window.setTimeout(setZ,400);
+    function setZ(){
+        $('#page'+pageNo).css('z-index',1);
+    }
+})
